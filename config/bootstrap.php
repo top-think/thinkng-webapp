e9919c99a0ceeb92e6bab4e44814f98fd5d6855b
slince <taosikai@yeah.net>
2016-03-25 16:17:37 +0800

    初始化仓库

---
+++ config/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+define('THINK_PACKAGE_PATH',  __DIR__ . '/../vendor/topthink/thinkng/');
+
+require __DIR__ . '/../vendor/autoload.php';
+require THINK_PACKAGE_PATH . '/src/start.php';
+
+date_default_timezone_set('Asia/shanghai');
