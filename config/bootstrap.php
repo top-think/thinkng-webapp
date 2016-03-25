dc6b3662c50db49895ed1be7c43ab2e6a1297693
slince <taosikai@yeah.net>
2016-03-25 18:04:04 +0800

    迁移部分文件位置

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,7 +1,11 @@
 <?php
-define('THINK_PACKAGE_PATH',  __DIR__ . '/../vendor/topthink/thinkng/');
-
+define('ROOT_PATH', __DIR__ . '/../');
+define('THINK_PATH',  __DIR__ . '/../vendor/topthink/thinkng/');
+//加载路径常量
+include __DIR__ . '/paths.php';
 require __DIR__ . '/../vendor/autoload.php';
-require THINK_PACKAGE_PATH . '/src/start.php';
 
+require THINK_PATH . '/src/start.php';
+
+//设置时间戳
 date_default_timezone_set('Asia/shanghai');
