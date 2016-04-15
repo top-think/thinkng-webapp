216f4e1b78e747ddbf4798979db8ea7b9bbc06e5
hainuo <admin@hainuo.info>
2016-04-15 16:03:07 +0800

    add 空方法和空控制器示例

--- application/Home/Controller/Pages.php
+++ application/Home/Controller/Pages.php
@@ -9,4 +9,18 @@ class Pages extends Controller
     {
         echo 'hello world';
     }
+
+    //空方法测试/Home/Pages/xx
+    public function _empty($name)
+    {
+        //把所有城市的操作解析到city方法
+        return $this->showCity($name);
+    }
+
+    //注意 showCity方法 本身是 protected 方法
+    protected function showCity($name)
+    {
+        //和$name这个城市相关的处理
+        return '当前城市' . $name;
+    }
 }
